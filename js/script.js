dbfb721acc9e09a199b6e7f97497bbace5f754d1
Joy Marocho <joy.marocho@student.moringaschool.com>
2022-03-04 16:25:15 +0300

    update js

--- js/script.js
+++ js/script.js
@@ -1,12 +1,27 @@
-// let gender = document.getElementById("gender");
-// let date =  document.getElementById("date");
-
+let gender, date;
 const form = document.getElementsByTagName("form")[0];
 
+let akan_name = document.getElementById('akan-name');
 
-
+// akan_name.innerHTML = "Dave"
 
 form.addEventListener("submit", function(e){
     e.preventDefault();
-    console.log("Submit...")
+    console.log("Submit...");
+    gender = form.elements["gender"].value;
+    date = form.elements["date"].value;
+    console.log(gender, date);
+    date = date.split("-");
+    console.log(date);
+
+    const cc = parseInt(date[0].slice(0, 2));
+    const yy = parseInt(date[0].slice(2));
+
+    const mm = parseInt(date[1]);
+
+    const dd = parseInt(date[2]);
+
+    console.log(cc, yy, mm, dd);
+
+
 })
