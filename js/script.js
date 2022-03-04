fb9140dda6554ec23d95ee04be1d0adff167a158
Joy Marocho <joy.marocho@student.moringaschool.com>
2022-03-04 16:43:38 +0300

    updatee if satement js

--- js/script.js
+++ js/script.js
@@ -3,7 +3,9 @@ const form = document.getElementsByTagName("form")[0];
 
 let akan_name = document.getElementById('akan-name');
 
-// akan_name.innerHTML = "Dave"
+// akan_name.innerHTML = "Joy"
+const male_akan = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
+const female_akan = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
 
 form.addEventListener("submit", function(e){
     e.preventDefault();
@@ -23,8 +25,17 @@ form.addEventListener("submit", function(e){
 
     let day = ( ( (cc/4) -2*cc-1) + ((5*yy/4) ) + ((26*(mm+1)/10)) + dd ) % 7;
 
-    day = Math.floor(day);
 
+    let akan;
+    day = Math.floor(day);
+    if(gender === "male"){
+        akan = male_akan[day]
+    }
+    else if(gender === "female"){
+        akan = female_akan[day];
+    }
+
+    akan_name.innerHTML = akan;
 
     console.log(day);
 
